b9d4415a909628c07ec4256f6e39e74571f141ea
flrngel <flrngel@gmail.com>
2015-09-06 23:56:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# fdenv
+
+print dotenv environmetns with format
+
+## Installation
+
+`npm install fdenv`
+
+## Usage
+
+```
+$ fdenv --help
+
+  Usage: fdenv [options]
+
+  Options:
+
+    -h, --help               output usage information
+    -V, --version            output the version number
+    -p, --prefix <string>    
+    -s, --suffix <string>    
+    -j, --joins <string>     
+    -f, --filepath <string>  set env path. defaults to .env
+
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var program = require("commander");
+var _ = require("underscore")
+
+program
+  .version("v1.0.0")
+  .usage("[options]")
+  .option("-p, --prefix <string>")
+  .option("-s, --suffix <string>")
+  .option("-j, --joins <string>")
+  .option("-f, --filepath <string>", 'set env path. defaults to .env', '.env')
+  .parse(process.argv);
+
+var dotenv = require("dotenv").config({path: program.filepath, returns: "Object"})
+var wrap_string = function(str){ return ((str)?(str):("")); };
+console.log(_.map(dotenv, function(value, key) { return wrap_string(program.prefix) + key + "=" + value + wrap_string(program.suffix) }).join( program.joins ));
